13c5681e741fdd8ff2b4eb4c9948ce97f9bd0293
Fernando Dodino <fernando.dodino@gmail.com>
2023-05-07 21:16:19 -0300

    update mongo 6 insert many

---
+++ scripts/02_generacion_datos_multiple.js
@@ -0,0 +1,110 @@
+db.viajes.insertMany([{
+    "fecha": "2017-04-23T21:53:43Z",
+    "chofer": {
+      "nombre": "Daniel",
+      "auto": {
+        "marca": "Fiat",
+        "modelo": "Siena",
+        "patente": "AB001RE",
+        "kilometraje": 45300
+      }
+    },
+    "cliente": {
+      "nombre": "Eduviges Peralta",
+      "numero": 791
+    },
+    "origen": "Soul Train",
+    "destino": "Lincoln y 25 de Mayo (San Martín)",
+    "costo": 50
+  },{
+    "fecha": "24/04/2017",
+    "chofer": {
+      "nombre": "Daniel",
+      "auto": {
+        "marca": "Fiat",
+        "modelo": "Siena",
+        "patente": "AB001RE",
+        "kilometraje": 45300
+      }
+    },
+    "cliente": {
+      "nombre": "Eduviges Peralta",
+      "numero": 791
+    },
+    "origen": "Pueyrredón y San Lorenzo (San Martín)",
+    "destino": "Lincoln y 25 de Mayo (San Martín)",
+    "costo": 75
+  },{
+    "fecha": "26/04/2017",
+    "chofer": {
+      "nombre": "Daniel",
+      "auto": {
+        "marca": "Fiat",
+        "modelo": "Siena",
+        "patente": "AB001RE",
+        "kilometraje": 45300
+      }
+    },
+    "cliente": {
+      "nombre": "Patricio García",
+      "numero": 2145
+    },
+    "origen": "Cancha de Chacarita",
+    "destino": "Estación de San Andrés",
+    "costo": 65
+  },{
+    "fecha": "02/05/2017",
+    "chofer": {
+      "nombre": "Daniel",
+      "auto": {
+        "marca": "Fiat",
+        "modelo": "Siena",
+        "patente": "AB001RE",
+        "kilometraje": 45300
+      }
+    },
+    "cliente": {
+      "nombre": "Eduviges Peralta",
+      "numero": 791
+    },
+    "origen": "Pueyrredón y San Lorenzo (San Martín)",
+    "destino": "Lincoln y 25 de Mayo (San Martín)",
+    "costo": 75
+  },{
+    "fecha": "24/04/2017",
+    "chofer": {
+      "nombre": "Verónica",
+      "auto": {
+        "marca": "VW",
+        "modelo": "Gol",
+        "patente": "ONG219",
+        "kilometraje": 50000
+      }
+    },
+    "cliente": {
+      "nombre": "Eduviges Peralta",
+      "numero": 791
+    },
+    "origen": "Pueyrredón y San Lorenzo (San Martín)",
+    "destino": "Lincoln y 25 de Mayo (San Martín)",
+    "costo": 75
+  },{
+    "fecha": "24/04/2017",
+    "chofer": {
+      "nombre": "Verónica",
+      "auto": {
+        "marca": "VW",
+        "modelo": "Gol",
+        "patente": "ONG219",
+        "kilometraje": 50000
+      }
+    },
+    "cliente": {
+      "nombre": "Patricio García",
+      "numero": 2145
+    },
+    "origen": "Estación de Malaver",
+    "destino": "Club de Paddle Las Piedras",
+    "costo": 140
+  }]);
+  
